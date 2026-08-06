df51c51951247f286382b894810a9a59517a3614
Aditya Yuvraj Patil <adityapatil2549@gmail.com>
2026-08-06 16:25:31 +0530

    Fix JSX syntax error in settings page

--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -188,7 +188,6 @@ export default function SettingsPage() {
                       Generate Export
                     </button>
                   </div>
-                  </div>
                 </div>
               </div>
 
