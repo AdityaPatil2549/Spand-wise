310e56ccf91c6218a7fa718b336a5fc9a90a8963
Aditya Yuvraj Patil <adityapatil2549@gmail.com>
2026-08-07 02:23:34 +0530

    Integrate RealFaviconGenerator zip assets

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -16,14 +16,16 @@ export default function manifest(): MetadataRoute.Manifest {
  type: 'image/x-icon',
  },
  {
- src: '/icon-192x192.png',
+ src: '/web-app-manifest-192x192.png',
  sizes: '192x192',
  type: 'image/png',
+ purpose: 'maskable',
  },
  {
- src: '/icon-512x512.png',
+ src: '/web-app-manifest-512x512.png',
  sizes: '512x512',
  type: 'image/png',
+ purpose: 'maskable',
  },
  ],
  };
